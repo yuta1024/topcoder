463b024e181b95686b425bc2d9a1b888bc010eae
yuta1024 <yuu725@gmail.com>
2012-12-21 02:49:48 +0900

    added SRM 565

---
+++ SRM565/MonstersValley.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <cstdio>
+#include <numeric>
+#include <bitset>
+#include <stack>
+#include <cstring>
+using namespace std;
+
+const int INF = 1 << 30;
+const double EPS = 1e-10;
+class MonstersValley
+{
+public:
+  vector<long long> dread;
+  vector<int> price;
+
+  int solve(int idx, long long sum)
+  {
+    if (idx >= (int)dread.size())
+      return 0;
+
+    int ret = solve(idx+1, sum+dread[idx]) + price[idx];
+    if (sum >= dread[idx])
+      ret = min(ret, solve(idx+1, sum));
+
+    return ret;
+  }
+  
+
+  int minimumPrice(vector<long long> dread, vector <int> price)
+  {
+    this->dread = dread;
+    this->price = price;
+
+    int ans = price[0];
+    vector<vector<long long> > dp(dread.size(), vector<long long>(1000, 0LL));
+    dp[0][price[0]] = dread[0];
+    for (unsigned int i = 1; i < dp.size(); ++i) {
+      for (unsigned int j = ans; j < dp[i].size(); ++j) {
+        dp[i][j] = max(dp[i][j], dp[i-1][j]);
+        if (j+price[i] < dp[i].size())
+          dp[i][j+price[i]] = max(dp[i][j+price[i]], dp[i-1][j] + dread[i]);
+      }
+      for (int j = ans; j < (int)dp[i].size(); ++j) {
+        if (dp[i][j] >= dread[i]) {
+          ans = j;
+          break;
+        }
+      }
+    }
+    return ans;
+  }
+  
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { long Arr0[] = {8, 5, 10}; vector<long long> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {1, 1, 2}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 2; verify_case(0, Arg2, minimumPrice(Arg0, Arg1)); }
+	void test_case_1() { long Arr0[] = {1, 2, 4, 1000000000}; vector<long long> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {1, 1, 1, 2}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 5; verify_case(1, Arg2, minimumPrice(Arg0, Arg1)); }
+	void test_case_2() { long Arr0[] = {200, 107, 105, 206, 307, 400}; vector<long long> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {1, 2, 1, 1, 1, 2}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 2; verify_case(2, Arg2, minimumPrice(Arg0, Arg1)); }
+	void test_case_3() { long Arr0[] = {5216, 12512, 613, 1256, 66, 17202, 30000, 23512, 2125, 33333}; vector<long long> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {2, 2, 1, 1, 1, 1, 2, 1, 2, 1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 5; verify_case(3, Arg2, minimumPrice(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+// BEGIN CUT HERE
+int main()
+{
+  MonstersValley ___test;
+   ___test.run_test(-1);
+}
+// END CUT HERE
